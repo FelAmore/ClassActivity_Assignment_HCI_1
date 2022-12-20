62059bf290958ccda1e0f8e8412842e509539d82
FelAmore <114073117+FelAmore@users.noreply.github.com>
2022-12-20 21:58:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//Please put your answer here
+$('#signup-modal').fadeIn()
+
+$('.signup-show').click(function() {
+	$('#signup-modal').fadeOut()
+}) 
+
+$('#close-modal').click(function() {
+	$('#signup-modal').fadeIn()
+}) 
